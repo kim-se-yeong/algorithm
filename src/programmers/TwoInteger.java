0e758589510d473ef29bd0854fcafdf432d0d3d8
seyeong.kim <kimseyeong97@gmail.com>
2022-01-24 23:02:13 +0900

    find the sum between two integers

---
+++ src/programmers/TwoInteger.java
@@ -0,0 +1,22 @@
+package programmers;
+
+/**
+ * 두 정수 a,b가 주어졌을 때 a와 b사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
+ * 예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+ */
+public class TwoInteger {
+    
+    public static void main(String[] args) {
+        
+        int a = 5, b = 3, sum = 0, temp = 0;
+        if (a > b) {
+            temp = b;
+            b = a;
+            a = (int)temp;
+        }
+        for (int i = a; i <= b; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
