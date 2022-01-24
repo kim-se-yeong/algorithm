a8128d90eb82be78ea41462239c84cada16fed55
seyeong.kim <kimseyeong97@gmail.com>
2022-01-24 22:49:47 +0900

    algorithm for increasing numbers by n

---
+++ src/programmers/IncreasingNumber.java
@@ -0,0 +1,22 @@
+package programmers;
+
+/**
+ * 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다.
+ * 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+ */
+public class IncreasingNumber {
+    
+    public static void main(String[] args) {
+        
+        long x = -4;
+        int n = 2;
+        long[] answer = new long[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = x + (x * i);
+        }
+
+        for (long a : answer) {
+            System.out.println(a);
+        }
+    }
+}
